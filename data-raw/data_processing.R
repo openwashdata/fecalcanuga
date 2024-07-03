385c3292f84909d0568ff755063ad1bb647fc4d7
Kelsey Shaw <kelseyshaw@uvic.ca>
2024-07-03 15:01:47 +0000

    Update readme and data_processing R script

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -3,16 +3,24 @@
 # Load packages ----------------------------------------------------------------
 ## Run the following code in console if you don't have the packages
 ## install.packages(c("usethis", "fs", "here", "readr", "openxlsx"))
+install.packages("readxl")
 library(usethis)
 library(fs)
 library(here)
 library(readr)
 library(openxlsx)
+library(readxl)
 
 # Read data --------------------------------------------------------------------
 # data_in <- read_csv("data-raw/dataset.csv")
 # codebook <- read_excel("data-raw/codebook.xlsx") |>
 #  clean_names()
+
+survey <- read_excel(here("data-raw", "rawdata_edited.xlsx"), sheet = 1)
+volaser <- read_excel(here("data-raw", "rawdata_edited.xlsx"), sheet = 2)
+gas <- read_excel(here("data-raw", "rawdata_edited.xlsx"), sheet = 3)
+parameters <- read_excel(here("data-raw", "rawdata_edited.xlsx"), sheet = 4)
+
 
 # Tidy data --------------------------------------------------------------------
 ## Clean the raw data into a tidy format here
@@ -20,8 +28,8 @@ library(openxlsx)
 
 # Export Data ------------------------------------------------------------------
 usethis::use_data(fecalcanuga, overwrite = TRUE)
-fs::dir_create(here::here("inst", "extdata"))
-readr::write_csv(fecalcanuga,
+fs::dir_create(here::here("inst", "extdata"))#readr::write_csv(fecalcanuga,
                  here::here("inst", "extdata", paste0("fecalcanuga", ".csv")))
 openxlsx::write.xlsx(fecalcanuga,
                      here::here("inst", "extdata", paste0("fecalcanuga", ".xlsx")))
+
