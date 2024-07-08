31f1445ce69f99b332a483d3b81f0fb591fa4eec
Kelsey Shaw <kelseyshaw@uvic.ca>
2024-07-08 09:04:53 +0000

    update code

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -2,7 +2,8 @@
 # R script to process uploaded raw data into a tidy, analysis-ready data frame
 # Load packages ----------------------------------------------------------------
 ## Run the following code in console if you don't have the packages
-## install.packages(c("usethis", "fs", "here", "readr", "openxlsx","readxl"))
+## install.packages(c("usethis", "fs", "here", "readr", "openxlsx","readxl",
+##"dplyr"))
 
 library(usethis)
 library(fs)
@@ -10,22 +11,48 @@ library(here)
 library(readr)
 library(openxlsx)
 library(readxl)
+library (dplyr)
 
 # Read data --------------------------------------------------------------------
 ## Read data from excel sheet into 4 separate data frames
 ##(household_survey_data, containment_data, ghg_data,phys_chem_parameter_data)
 
-household_survey_data <- read_excel(here("data-raw", "rawdata_edited.xlsx"), sheet = 1)
-containment_data <- read_excel(here("data-raw", "rawdata_edited.xlsx"), sheet = 2)
-ghg_data <- read_excel(here("data-raw", "rawdata_edited.xlsx"), sheet = 3)
-phys_chem_parameter_data <- read_excel(here("data-raw", "rawdata_edited.xlsx"), sheet = 4)
+household_survey_data <- read_excel(here("data-raw", "rawdata_edited.xlsx"),
+                                    sheet = 1)
+containment_data <- read_excel(here("data-raw", "rawdata_edited.xlsx"),
+                               sheet = 2)
+ghg_data <- read_excel(here("data-raw", "rawdata_edited.xlsx"),
+                       sheet = 3)
+phys_chem_parameter_data <- read_excel(here("data-raw", "rawdata_edited.xlsx"),
+                                       sheet = 4)
 
 
 # Tidy data --------------------------------------------------------------------
 ## Clean the raw data into a tidy format here
 ### Change data types
-### Missing Values
+# Get the class (data type) of each column
+sapply(containment_data, class)
+sapply(ghg_data, class)
+sapply(household_survey_data, class)
+sapply(phys_chem_parameter_data, class)
 
+# Convert all character columns to factors, and fix colums that should be
+##read in as numeric
+containment_data <- containment_data %>% mutate_if(is.character, as.factor)
+ghg_data <- ghg_data %>% mutate_if(is.character, as.factor)
+household_survey_data <- household_survey_data %>% mutate_if(is.character,
+                                                             as.factor)
+phys_chem_parameter_data$TOC <- as.numeric(phys_chem_parameter_data$TOC)
+phys_chem_parameter_data <- phys_chem_parameter_data %>% mutate_if(is.character,
+                                                                   as.factor)
+
+# View the structure of the dataframe to verify changes
+str(containment_data)
+str(ghg_data)
+str(household_survey_data)
+str(phys_chem_parameter_data)
+
+### Missing Values
 
 # Export Data ------------------------------------------------------------------
 ## Save data within package data directory
